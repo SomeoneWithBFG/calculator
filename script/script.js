19ecd650631bae31f522065212a425ed79a37d40
SomeoneWithBFG <klastorka281000@gmail.com>
2020-02-20 17:49:57 +0300

    day3

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+const DAY_STRING = ['день','дня','дней'];
+
 const DATA = {
     whichSite: ['landing','multiPage','onlineStore'],
     price: [4000,8000,26000],
@@ -14,13 +16,17 @@ const DATA = {
 };
 
 const startButton = document.querySelector('.start-button'),
-    firstScreen = document.querySelector('.first-screen'),
-    mainForm = document.querySelector('.main-form'),
-    formCalculate = document.querySelector('.form-calculate'),
-    endButton = document.querySelector('.end-button'),
-    total = document.querySelector('.total'),
-    fastRange = document.querySelector('.fast-range'),
-    totalPriceSum = document.querySelector('.total_price__sum');
+      firstScreen = document.querySelector('.first-screen'),
+      mainForm = document.querySelector('.main-form'),
+      formCalculate = document.querySelector('.form-calculate'),
+      endButton = document.querySelector('.end-button'),
+      total = document.querySelector('.total'),
+      fastRange = document.querySelector('.fast-range'),
+      totalPriceSum = document.querySelector('.total_price__sum'),
+      typeSite = document.querySelector('.type-site'),
+      maxDeadline = document.querySelector('.max-deadline'),
+      rangeDeadline = document.querySelector('.range-deadline'),
+      deadlineValue = document.querySelector('.deadline-value');
 
 function showElem(elem) {
     elem.style.display = 'block';
@@ -30,10 +36,28 @@ function hideElem(elem) {
     elem.style.display = 'none';
 }
 
+function renderTextContent(total, site, maxDay, minDay) {
+    typeSite.textContent = site;
+    totalPriceSum.textContent = total;
+    maxDeadline.textContent = declOfNum(maxDay, DAY_STRING);
+    rangeDeadline.min = minDay;
+    rangeDeadline.max = maxDay;
+    deadlineValue.textContent = declOfNum(rangeDeadline.value, DAY_STRING);
+}
+
+function declOfNum(n, titles) {
+    return n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
+                            0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+}
+
 function priceCalculation(elem) {
     let result = 0,
         index = 0,
-        options = [];
+        options = [],
+        site = '',
+        maxDeadlineDay = DATA.deadlineDay[index][1],
+        minDeadlineDay = DATA.deadlineDay[index][0];
+
     if(elem.name === 'whichSite') {
         for(const item of formCalculate.elements) {
            if (item.type === 'checkbox') {
@@ -46,6 +70,9 @@ function priceCalculation(elem) {
     for(const item of formCalculate.elements) {
         if(item.name === 'whichSite' && item.checked) {
             index = DATA.whichSite.indexOf(item.value);
+            site = item.dataset.site;
+            maxDeadlineDay = DATA.deadlineDay[index][1];
+            minDeadlineDay = DATA.deadlineDay[index][0];
         } else if(item.classList.contains('calc-handler') && item.checked) {
             options.push(item.value);
         }
@@ -68,8 +95,7 @@ function priceCalculation(elem) {
     });
 
     result += DATA.price[index];
-
-    totalPriceSum.textContent = result;
+    renderTextContent(result, site, maxDeadlineDay, minDeadlineDay)
 }
 
 function handlerCallBackForm() {
@@ -87,6 +113,8 @@ function handlerCallBackForm() {
     }
 }
 
+
+
 startButton.addEventListener('click', function() {
     showElem(mainForm);
     hideElem(firstScreen);
@@ -100,5 +128,33 @@ endButton.addEventListener('click',function(){
     }
     showElem(total);
 });
+
+desktopTemplates.addEventListener('click', () => {
+    if (desktopTemplates.checked)
+        document.querySelector('.checkbox-label.desktopTemplates_value').textContent = "Да";
+    else
+        document.querySelector('.checkbox-label.desktopTemplates_value').textContent = "Нет";
+})
+
+adapt.addEventListener('click', () => {
+    if (adapt.checked)
+        document.querySelector('.checkbox-label.adapt_value').textContent = "Да";
+    else
+        document.querySelector('.checkbox-label.adapt_value').textContent = "Нет";
+})
+
+mobileTemplates.addEventListener('click', () => {
+    if (mobileTemplates.checked)
+        document.querySelector('.checkbox-label.mobileTemplates_value').textContent = "Да";
+    else
+        document.querySelector('.checkbox-label.mobileTemplates_value').textContent = "Нет";
+})
+
+editable.addEventListener('click', () => {
+    if (mobileTemplates.checked)
+        document.querySelector('.checkbox-label.editable_value').textContent = "Да";
+    else
+        document.querySelector('.checkbox-label.editable_value').textContent = "Нет";
+})
 
 formCalculate.addEventListener('change',handlerCallBackForm);
